e9dbffbde67cad4548c25579433ed6787cde5b23
Rohit <rohityadav2604@gmail.com>
2021-06-28 00:29:27 +0530

    quicksortdone

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const ArrayContainer = document.querySelector(".array-container");
 const Generate = document.querySelector(".generate");
 const SortButton = document.querySelector(".selection-sort");
 const BubbleSort = document.querySelector(".bubble-sort");
-const MergeSort = document.querySelector(".merge-sort");
+//const MergeSort = document.querySelector(".merge-sort");
+const Quicksort = document.querySelector(".quick-sort");
 
+
+
+
+
+SortButton.addEventListener("click" , SelectionSort);
+BubbleSort.addEventListener("click" , bubblesort );
+//MergeSort.addEventListener("click" , mergesort);
+Quicksort.addEventListener("click" , quick )
 
 Generate.addEventListener("click" , ShowArray);
 function ShowArray()
@@ -21,9 +30,7 @@ function ShowArray()
         
     }
 }
-SortButton.addEventListener("click" , SelectionSort);
-BubbleSort.addEventListener("click" , bubblesort );
-MergeSort.addEventListener("click" , mergesort);
+
    
 
 async function SelectionSort(e)
@@ -53,12 +60,8 @@ async function SelectionSort(e)
          }
 
          arr[i].classList.remove("activeclass");
-         let minvalue = arr[minidx].textContent;
-         let minvalueheight = arr[minidx].style.height;
-         arr[minidx].textContent = arr[i].textContent;
-         arr[minidx].style.height = arr[i].style.height;
-         arr[i].style.height = minvalueheight;
-         arr[i].textContent = minvalue;
+        
+        swap(arr ,minidx , i);
          arr[i].classList.add("activeclass3");
 
 
@@ -87,14 +90,9 @@ async function bubblesort()
 
              if(Number(arr[j].textContent) > Number(arr[j+1].textContent))
              {
-                 let minidx = j+1;
+                 
+                swap(arr , j+1  ,  j);
                 
-                let minvalue = arr[minidx].textContent;
-                let minvalueheight = arr[minidx].style.height;
-                arr[minidx].textContent = arr[j].textContent;
-                arr[minidx].style.height = arr[j].style.height;
-                arr[j].style.height = minvalueheight;
-                arr[j].textContent = minvalue;
                 
                  
              }
@@ -109,33 +107,229 @@ async function bubblesort()
     }
 }
 
+async function partiton(arr , value , start , end)
+{
+    console.log("i am ");
+    let i = start;
+    let j = start;
+    while(j<=end)
+    {
+         if(Number(arr[j].textContent) > Number(value.textContent))
+         {
+             j++;
+         }
+         else
+         {
+            arr[j].classList.add("activeclass")
+            arr[i].classList.add("activeclass2");
+           await new Promise((res) =>{
+               setTimeout(()=>{
+                    res();
+               } , 50)
+           });
+            await swap(arr , j , i);
+            arr[j].classList.remove("activeclass")
+            arr[i].classList.remove("activeclass2");
+            i++;
+            j++;
+         }
+    }
+    console.log(i);
+    arr[i-1].classList.add("activeclass3");
+    return i-1;
+}
+async function quicksortarray(arr , start , end)
+{
+    // console.log(start);
+    // console.log(end);
+      if(start>end)
+      {
+          return;
+      }
+
+      let pivot = await partiton(arr , arr[end] , start , end);
+      
+     await quicksortarray(arr , start , pivot-1);
+     await quicksortarray(arr  , pivot+1 , end);
+}
+
+function quick()
+{
+    let arr = document.querySelectorAll(".valueDiv");
+    let start = 0;
+    let end = parseInt(arr.length-1);
+    quicksortarray(arr ,start , end);
+}
 
 
-// function merge(arr , start , end)
-// {
+
+
+
+
+
+
+
+
+
+
+async function sortarray(arr , start , mid , end)
+{
+  
     
-//     start = Number(start);
-//     end = Number(end);
-//     if(start>=end)
-//     {
-//         return;
-//     }
-//     let mid = parseInt(start+(end-start)/2);
-//     mid = parseInt(mid);
-//     console.log(mid);
-//     merge(arr , start , mid);
-//     merge(arr , mid+1 , end);
+    let FirstArray_Index = start;
+    let SecondArray_Index = mid+1;
+    let temp = [];
+   
+    while(FirstArray_Index<=mid && SecondArray_Index<=end)
+    {
+          
+           if(Number(arr[FirstArray_Index].textContent) < Number(arr[SecondArray_Index].textContent))
+           {
+              console.log(arr[FirstArray_Index]);
+               arr[FirstArray_Index].classList.add("activeclass");
+               arr[SecondArray_Index].classList.add("activeclass2");
+               await new Promise((res) =>{
+                setTimeout(()=>{
+                     res();
+                } , 10)
+            });
 
-// }
+               temp.push(arr[FirstArray_Index]);
+               arr[FirstArray_Index].classList.remove("activeclass");
+               arr[SecondArray_Index].classList.remove("activeclass2");
+               FirstArray_Index++;
 
-// function mergesort()
-// {
+           }
+           else if(Number(arr[FirstArray_Index].textContent) > Number(arr[SecondArray_Index].textContent))
+           {
+            console.log(arr[SecondArray_Index]);
+                arr[FirstArray_Index].classList.add("activeclass2");
+                arr[SecondArray_Index].classList.add("activeclass");
+                await new Promise((res) =>{
+                    setTimeout(()=>{
+                         res();
+                    } , 10)
+                });
+   
+                temp.push(arr[SecondArray_Index]);
+                arr[FirstArray_Index].classList.remove("activeclass2");
+                arr[SecondArray_Index].classList.remove("activeclass");
+                SecondArray_Index++;      
+           }
+           else
+           {
+                  arr[FirstArray_Index].classList.add("activeclass2");
+                  arr[SecondArray_Index].classList.add("activeclass");
+                  await new Promise((res) =>{
+                    setTimeout(()=>{
+                         res();
+                    } , 10)
+                });
+   
+                  temp.push(arr[SecondArray_Index]);
+                  temp.push(arr[FirstArray_Index]);
+                 arr[FirstArray_Index].classList.remove("activeclass2");
+                 arr[SecondArray_Index].classList.remove("activeclass");
+                SecondArray_Index++;  
+                FirstArray_Index++;
+           }
+    }
+
+
+    while(FirstArray_Index<=mid)
+    {
+        arr[FirstArray_Index].classList.add("activeclass");
+        await new Promise((res) =>{
+            setTimeout(()=>{
+                 res();
+            } , 1000)
+        });
+
+        temp.push(arr[FirstArray_Index]);
+        arr[FirstArray_Index].classList.remove("activeclass");
+        FirstArray_Index++;
+
+
+    }
+    while(SecondArray_Index<=end)
+    {
+        arr[SecondArray_Index].classList.add("activeclass");
+        await new Promise((res) =>{
+            setTimeout(()=>{
+                 res();
+            } , 10)
+        });
+
+        temp.push(arr[SecondArray_Index]);
+        arr[SecondArray_Index].classList.remove("activeclass");
+        SecondArray_Index++;
+    }
+    let k = start
+    for(let i = 0 ; i<temp.length;i++)
+    {
+       
+        temp[i].classList.add("activeclass3");
+        arr[k++] = temp[i];
+        
+    }
+    temp = [];
+
+}
+
+async function merge(arr , start , end)
+{
     
-//     let arr = document.querySelectorAll(".valueDiv");
-//     let start =0;
-//     let end = arr.length;
+    start = Number(start);
+    end = Number(end);
+    if(start>=end)
+    {
+        return new Promise((res) =>{
+            setTimeout(()=>{
+                 res();
+            } , 10)
+        });
+    }
+    let mid = parseInt(start+(end-start)/2);
+    mid = parseInt(mid);
+   // console.log(mid);
+    await merge(arr , start , mid);
+     await merge(arr , mid+1 , end);
+     console.log(start);
+     console.log(mid);
+     console.log(end);
+    await sortarray(arr , start , mid ,  end);
 
+}
+
+function mergesort()
+{
+    
+    let arr = document.querySelectorAll(".valueDiv");
+    let start =0;
+    let end = arr.length-1;
+   // console.log(end)
 
        
-//     merge(arr , start , end);
-// }
+    merge(arr , start , end);
+}
+
+
+
+function swap(arr , first  , second)
+{
+
+                // let minvalue = arr[minidx].textContent;
+                // let minvalueheight = arr[minidx].style.height;
+                // arr[minidx].textContent = arr[j].textContent;
+                // arr[minidx].style.height = arr[j].style.height;
+                // arr[j].style.height = minvalueheight;
+                // arr[j].textContent = minvalue;
+
+                let minvalue = arr[first].textContent;
+                let minvalueheight = arr[first].style.height;
+                arr[first].textContent = arr[second].textContent;
+                arr[first].style.height = arr[second].style.height;
+                arr[second].style.height = minvalueheight;
+                arr[second].textContent = minvalue;
+
+}
